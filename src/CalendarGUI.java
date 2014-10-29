84a99a784ce4d20cf7a8a7cc7fd69ddd4e0754dc
aakov-dy <aakov-dy@192.168.239.1>
2014-10-30 07:50:12 +0800

    SWDESPA_DC1

--- src/CalendarGUI.java
+++ src/CalendarGUI.java
@@ -27,40 +27,46 @@ public class CalendarGUI {
         mainWindow.setResizable(false);
         
         mainPanel = new JPanel(null);
+        mainPanel.setBounds(0, 0, 640, 670);
         
         monthLabel = new JLabel("January");
         monthLabel.setBounds(320 - monthLabel.getPreferredSize().width / 2, 50, 200, 50);
+        mainPanel.add(monthLabel);
         
         yearLabel = new JLabel("Change Year:");
         yearLabel.setBounds(350, 610, 160, 40);
+        mainPanel.add(yearLabel);
         
         cmbYear = new JComboBox();
         cmbYear.setBounds(460, 610, 160, 40);
+        mainPanel.add(cmbYear);
         
         btnPrev = new JButton("<<");
         btnPrev.setName("btnPrev");
         btnPrev.setBounds(20, 50, 100, 50);
+        mainPanel.add(btnPrev);
         
         btnNext = new JButton(">>");
         btnNext.setName("btnNext");
         btnNext.setBounds(520, 50, 100, 50);
+        mainPanel.add(btnNext);
         
-        btnImport = new JButton("Import Event");
+        btnImport = new JButton("<html><center>Import<br>Event</center></html>"); //Use HTML codes to format text
         btnImport.setName("btnImport");
         btnImport.setBounds(20, 610, 100, 50);
+        mainPanel.add(btnImport);
         
-        btnFBNotif = new JButton("Send notifications to Facebook");
+        btnFBNotif = new JButton("<html><center>Send to<br>Facebook</center></html>");
         btnFBNotif.setName("btnFBNotif");
         btnFBNotif.setBounds(130, 610, 100, 50);
+        mainPanel.add(btnFBNotif);
         
-        btnSMSNotif = new JButton("Send notification to SMS");
+        btnSMSNotif = new JButton("<html><center>Send to<br>SMS</center></html>");
         btnSMSNotif.setName("btnSMSNotif");
         btnSMSNotif.setBounds(240, 610, 100, 50);
+        mainPanel.add(btnSMSNotif);
         
-        calendarPanel = new JPanel(null);
-        calendarPanel.setBounds(0, 0, 640, 670);
-        
-        
+        mainPanel.setBorder(BorderFactory.createTitledBorder("Calendar"));
         mainWindow.add(mainPanel);
         
         mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
